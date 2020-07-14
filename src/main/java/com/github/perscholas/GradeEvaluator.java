ad177d3da486b7f6b171df28891eee27929476fa
CJ Fulton <fulchr3356@gmail.com>
2020-07-14 11:31:22 -0400

    Finished Exercise and fixed test case

--- src/main/java/com/github/perscholas/GradeEvaluator.java
+++ src/main/java/com/github/perscholas/GradeEvaluator.java
@@ -5,26 +5,38 @@ package com.github.perscholas;
  */
 public class GradeEvaluator {
     public Boolean isGradeA(Double score) {
-        return null;
+        return score >= 90;
     }
 
     public Boolean isGradeB(Double score) {
-        return null;
+        return (score >= 80 && score <90);
     }
 
     public Boolean isGradeC(Double score) {
-        return null;
+        return (score >= 70 && score <80);
     }
 
     public Boolean isGradeD(Double score) {
-        return null;
+        return (score >= 65 && score <=69);
     }
 
     public Boolean isGradeF(Double score) {
-        return null;
+
+        return score <=64;
     }
 
     public Character getGrade(Double score) {
-        return null;
+
+        if (isGradeA(score))
+            return 'A';
+        else if (isGradeB(score))
+            return 'B';
+        else if(isGradeC(score))
+            return 'C';
+        else if (isGradeD(score))
+            return 'D';
+        else if (isGradeF(score))
+            return 'F';
+        return 'F';
     }
 }
